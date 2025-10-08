9b50c25fcd8fbf85bbef022f5d2dc88d1fee4ce4
rk-28 <93245565+rk-28@users.noreply.github.com>
2025-10-09 00:22:29 +0530

    Update update_k8s_manifests.groovy

--- vars/update_k8s_manifests.groovy
+++ vars/update_k8s_manifests.groovy
@@ -12,10 +12,7 @@ def call(Map config = [:]) {
     
     echo "Updating Kubernetes manifests with image tag: ${imageTag}"
     
-    withCredentials([usernamePassword(
-        credentialsId: 'github-credentials',
-        usernameVariable: 'GIT_USERNAME',
-        passwordVariable: 'GITHUB_TOKEN'
+withCredentials([string(credentialsId: 'github-pat', variable: 'GITHUB_TOKEN'
     )]) {
         // Configure Git
         sh """
@@ -47,7 +44,7 @@ def call(Map config = [:]) {
                 git commit -m "Update image tags to ${imageTag} and ensure correct domain [ci skip]"
                 
                 # Set up credentials for push
-                git remote set-url origin https://\${GIT_USERNAME}:\${GITHUB_TOKEN}@github.com/rk-28/eks_project.git
+                git remote set-url origin https://rk-28:\${GITHUB_TOKEN}@github.com/rk-28/eks_project.git
                 git push origin HEAD:\${GIT_BRANCH}
             fi
         """
